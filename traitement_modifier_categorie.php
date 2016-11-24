656ba9e33cc16b6a1a752b24f7adfa27296bb3dd
Saphizia <miss-du-1.9@live.fr>
2016-11-24 08:26:52 +0100

    Add files via upload

--- traitement_modifier_categorie.php
+++ traitement_modifier_categorie.php
@@ -1,11 +1,16 @@
+<meta charset="utf-8">
 <?php
 include 'include/bdd.inc.php';
 include 'include/class_cat.inc.php';
-$catid = $_POST['id'];
-$catlib = $_POST['lib'];
-$lacat = new categorie('','');
-while($catid)
-{   
-$lacat->maj_categorie($catid,$catlib,$conn);
+$lacat = new categorie('','',$conn);
+$resa = $lacat->afficher_categorie($conn);
+while($tabcat =$resa->fetch()) 
+{ 
+    $namelib = "libcat".$tabcat->idcat;
+    echo $namelib;
+    $lib = $_POST[$namelib];
+    echo '   '.$lib;
+    
+    $lacat->maj_categorie($tabcat->idcat,$lib,$conn);
 }
 ?>
